726e9a4b80f6d3ee6209d03ae4f6dd0645f17b11
HarryDang <danghuuphuc001@gmail.com>
2024-03-06 00:20:19 +0700

    feat: Drag && Drop for Lists and Cards in the Board

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Draggable } from "@hello-pangea/dnd";
+
 import { Card } from "@prisma/client";
 
 interface CardItemProps {
@@ -9,11 +11,22 @@ interface CardItemProps {
 
 export const CardItem = ({ data, index }: CardItemProps) => {
   return (
-    <div
-      role="button"
-      className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
-    >
-      {data.title}
-    </div>
+    <Draggable draggableId={data.id} index={index}>
+      {(provided) => {
+        // console.log(provided);
+        return (
+          <div
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            // the ref will be dragged or dropped
+            ref={provided.innerRef}
+            role="button"
+            className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
+          >
+            {data.title}
+          </div>
+        );
+      }}
+    </Draggable>
   );
 };
